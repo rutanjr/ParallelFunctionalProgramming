9ea2f28b17f4333a08e02d978840efec02d7b7f9
Oskar Rutqvist <rutanjr@gmail.com>
2018-03-28 15:45:00 +0200

    first version par and depth par

--- Lab1/Labxx.hs
+++ Lab1/Labxx.hs
@@ -1,13 +1,51 @@
-module Labxx where
+module Main where
 
 import Data.List
 import System.Random
 import Criterion.Main
+import Control.Parallel
 
 -- code borrowed from the Stanford Course 240h (Functional Systems in Haskell)
 -- I suspect it comes from Bryan O'Sullivan, author of Criterion
 
 data T a = T !a !Int
+
+-- | Parallel constructs working in a certain depth.
+------------------------------------------------------------------------------
+
+
+-- * jacknife calling depth par map.
+depthparjack :: Int -> ([a] -> b) ->  [a] -> [b]
+depthparjack d f = (depthparmap d f) . resamples 500
+
+-- * Depth parametrized map.
+depthparmap :: Int -> (a -> b) ->  [a] -> [b]
+depthparmap _ _ [] = []
+depthparmap d f (x : xs) =
+  if d > 0 then par middle $ pseq rest first ++ middle ++ rest
+           else [f x] ++ map f xs
+     where first = [f x]
+           middle = parmap f (take  (length xs `div` 2) xs)
+           rest = parmap f (drop  (length xs `div` 2) xs)
+           
+-- | Parallel constructs, just sparks til the end of time.
+------------------------------------------------------------------------------
+
+-- * Parallel jacknife, calling the strictly parallel map.
+parjack :: ([a] -> b) -> [a] -> [b]
+parjack f = parmap f . resamples 500
+
+
+-- * Parallel map. Sparks everywhere.
+parmap :: (a -> b) -> [a] -> [b]
+parmap f [] = []
+parmap f (x:xs) = par middle $
+               pseq rest
+                first ++ middle ++ rest
+  where
+    first = [f x]
+    middle = parmap f (take  (length xs `div` 2) xs)
+    rest = parmap f (drop  (length xs `div` 2) xs)
 
 
 mean :: (RealFrac a) => [a] -> a
@@ -43,6 +81,8 @@ main = do
   putStrLn $ "jack mean min:  " ++ show (minimum j)
   putStrLn $ "jack mean max:  " ++ show (maximum j)
   defaultMain
-        [
-         bench "jackknife" (nf (jackknife  mean) rs)
-         ]
+        [ bench "jackknife" (nf (jackknife  mean) rs)
+        , bench "parjack" (nf (parjack  mean) rs)
+        , bench "depthparjack" (nf (depthparjack 2 mean) rs)
+        ]
+
