22a49ac8ada1695ac7949f572f7b53f0c6a4dac1
Oskar Rutqvist <rutanjr@gmail.com>
2018-04-09 16:57:50 +0200

    first part of the lab complete

--- Lab1/Labxx.hs
+++ Lab1/Labxx.hs
@@ -4,50 +4,69 @@ import Data.List
 import System.Random
 import Criterion.Main
 import Control.Parallel
+import Control.Parallel.Strategies
+import Control.Monad.Par hiding (parMap)
+--import Control.DeepSeq
+
 
 -- code borrowed from the Stanford Course 240h (Functional Systems in Haskell)
 -- I suspect it comes from Bryan O'Sullivan, author of Criterion
 
 data T a = T !a !Int
 
--- | Parallel constructs working in a certain depth.
-------------------------------------------------------------------------------
-
-
--- * jacknife calling depth par map.
-depthparjack :: Int -> ([a] -> b) ->  [a] -> [b]
-depthparjack d f = (depthparmap d f) . resamples 500
-
--- * Depth parametrized map.
-depthparmap :: Int -> (a -> b) ->  [a] -> [b]
-depthparmap _ _ [] = []
-depthparmap d f (x : xs) =
-  if d > 0 then par middle $ pseq rest first ++ middle ++ rest
-           else [f x] ++ map f xs
-     where first = [f x]
-           middle = parmap f (take  (length xs `div` 2) xs)
-           rest = parmap f (drop  (length xs `div` 2) xs)
-           
--- | Parallel constructs, just sparks til the end of time.
-------------------------------------------------------------------------------
-
 -- * Parallel jacknife, calling the strictly parallel map.
-parjack :: ([a] -> b) -> [a] -> [b]
-parjack f = parmap f . resamples 500
+parjack :: (([a] -> b) -> [[a]] -> [b]) -> ([a] -> b) -> [a] -> [b]
+parjack mapfun f = mapfun f . resamples 500
 
 
--- * Parallel map. Sparks everywhere.
-parmap :: (a -> b) -> [a] -> [b]
-parmap f [] = []
-parmap f (x:xs) = par middle $
+-- | Parallel construct, using Control.Parallel (par,pseq),
+-- just sparks til the end of time.
+------------------------------------------------------------------------------
+parPseqMap :: (a -> b) -> [a] -> [b]
+parPseqMap f [] = []
+parPseqMap f (x:xs) = par first $
                pseq rest
-                first ++ middle ++ rest
+                (first : rest)
   where
-    first = [f x]
-    middle = parmap f (take  (length xs `div` 2) xs)
-    rest = parmap f (drop  (length xs `div` 2) xs)
+    first = f x
+    --middle = parmap f (take  (length xs `div` 2) xs)
+    rest = parPseqMap f xs -- (drop  (length xs `div` 2) xs)
 
+-- | Eval monad parallel map.
+------------------------------------------------------------------------------
+evalMap :: (a -> b) -> [a] -> [b]
+evalMap _ [] = []
+evalMap f (x:xs) = runEval $ do
+  first <- rpar (f x)
+  rest  <- rseq (evalMap f xs)
+  return (first : rest)
 
+-- | Par Monad implementation
+------------------------------------------------------------------------------
+parMonadMapM :: NFData b => (a -> b) -> [a] -> Par [b]
+parMonadMapM _ [] = return []
+parMonadMapM f xs = do
+  is <- sequence . replicate (length xs) $ new 
+  let ys = map f xs
+  sequence $ map fork $ zipWith put is ys
+  mapM get is
+  
+
+parMonadMap :: NFData b => (a -> b) -> [a] -> [b]
+parMonadMap f = runPar. parMonadMapM f
+
+-- | Strategy implemenation
+------------------------------------------------------------------------------
+type Strategy' a = a -> ()
+
+parList' :: Strategy' a -> Strategy' [a]
+parList' s [] = ()
+parList' s (x:xs) = s x `par` parList' s xs
+
+stratMap ::(a -> b) -> [a] -> [b]
+stratMap f xs = map f xs `using` parList rwhnf 
+
+------------------------------------------------------------------------------
 mean :: (RealFrac a) => [a] -> a
 mean = fini . foldl' go (T 0 0)
   where
@@ -82,7 +101,9 @@ main = do
   putStrLn $ "jack mean max:  " ++ show (maximum j)
   defaultMain
         [ bench "jackknife" (nf (jackknife  mean) rs)
-        , bench "parjack" (nf (parjack  mean) rs)
-        , bench "depthparjack" (nf (depthparjack 2 mean) rs)
+        , bench "parjack" (nf (parjack parPseqMap mean) rs)
+        , bench "evaljack" (nf (parjack evalMap mean) rs)
+        , bench "stratjack" (nf (parjack stratMap mean) rs)
+        , bench "parMonadjack" (nf (parjack parMonadMap mean) rs)
         ]
 
